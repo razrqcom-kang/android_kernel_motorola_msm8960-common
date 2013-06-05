639bd447ab5e2f484a3a16a64f0392318141c4ad
dhacker29 <davidhackerdvm@gmail.com>
2013-06-05 02:57:11 -0500

    board-mmi-pmic: Changes from "Update the FCC learning algorithm"
    Caf commit 13e6d4fb311aea48a09f59623073991f6d0418da
    
    Change-Id: I1e7d33a1f951e954be798bbe699d65db21d97a43

--- arch/arm/mach-msm/board-mmi-pmic.c
+++ arch/arm/mach-msm/board-mmi-pmic.c
@@ -665,6 +665,10 @@ static struct pm8921_bms_platform_data pm8921_bms_pdata __devinitdata = {
 	.high_ocv_correction_limit_uv	= 50,
 	.low_ocv_correction_limit_uv	= 100,
 	.hold_soc_est			= 3,
+	.enable_fcc_learning = 1,
+	.min_fcc_learning_soc = 20,
+	.min_fcc_ocv_pc = 30,
+	.max_fcc_learning_samples = 5,
 #ifdef CONFIG_PM8921_EXTENDED_INFO
 	.get_batt_info          = read_mmi_battery_bms,
 #endif
